9771a5970d86157004faea455f700d430ac5506c
IvannaCheban <ivanna_cheban@ukr.net>
2023-06-13 18:46:16 +0300

    implemented smooth scrolig to elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
