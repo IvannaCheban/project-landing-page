0ad67eb4827a449b0111a937ab32277dcb56fe11
IvannaCheban <ivanna_cheban@ukr.net>
2023-06-14 14:14:45 +0300

    implemented tabbed contenet functionality

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+const tabs = document.querySelectorAll(".quotes_tab");
+const tabsContainer = document.querySelector(".quotes__tab--container");
+const tabsContent = document.querySelectorAll(".quotes__content");
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".quotes__tab"); //selecting common parrent of elements insite the button
+
+  //Guard clause
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("quotes__tab--active")); //removing active class from every tab
+
+  clicked.classList.add("quotes__tab--active"); // adding class list to clicked element
+
+  tabsContent.forEach((c) => c.classList.remove("quotes__content--active")); //remoivng active class from all components
+  document
+    .querySelector(`.quotes__content--${clicked.dataset.tab}`)
+    .classList.add("quotes__content--active");
+});
